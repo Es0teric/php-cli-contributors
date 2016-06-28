d7806236125e2e027acf066163644dddc33bf52a
Leo Fulgencio <leo.fulgencio@gmail.com>
2016-06-28 17:17:34 -0400

    updated contributors file in root dir

--- contributors.php
+++ contributors.php
@@ -1,49 +1,17 @@
 #!/usr/bin/env php
 <?php
 
-require_once "Storage.php";
-require_once "Controllers/QuitController.php";
-require_once "Controllers/AddController.php";
-require_once "Router.php";
+use App\Application;
 
-class Application {
+class Contributors {
 
-    public $welcome = "Type your message. Type 'quit' on a line by itself when you're done.\n";
-    public $open = true;
-    public $storage;
-
-    public function __construct() {
-
-        $this->storage = new Storage();
+    public function init() {
         
-        //need to make storage (single instance) like a service and accessible from the controllers (ioc)
-        $this->router = new Router();
+        //lets init the application and begin
+        $app = new Application->run();
 
-        // set routes here or hardcode on 
     }
 
-    public function run() {
-
-        print $this->welcome;
-        
-        while( $this->open ) {
-
-            $fp = fopen('php://stdin', 'r');
-            $input = fgets( $fp, 1024 ); // read the special file to get the user input from keyboard            
-            $inputs = $this->parse($input);
-            $this->handle($input);
-            
-        }
-    }
-
-    public function parse($input) {
-        // general validate
-        return explode(" ", $input);
-    }
-
-    public function handle($input) {
-        $this->router->handle(trim($input));
-    }
 }
-$app = new Application;
-$app->run();
+$contributors = new Contributors;
+$contributors->init();
